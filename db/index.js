1583f256054704d42689d15db714133afccf8808
Han Chiang <yaphc@hotmail.com>
2020-08-02 11:14:06 +0800

    Fix and update db config file. Remove old file

--- db/index.js
+++ db/index.js
@@ -1,11 +1,15 @@
 /* eslint-disable prefer-destructuring */
 const { Sequelize } = require('sequelize');
+const cls = require('cls-hooked');
+
+const namespace = cls.createNamespace('my-very-own-namespace');
+Sequelize.useCLS(namespace);
 
 const { initUser, User } = require('./models/user');
 const { initPost, Post } = require('./models/post');
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.HOST,
+  host: process.env.DB_HOST,
   dialect: 'mysql'
 });
 
@@ -13,5 +17,5 @@ initUser(sequelize);
 initPost(sequelize);
 
 module.exports = {
-  User, Post
-}
+  User, Post, sequelize
+};
