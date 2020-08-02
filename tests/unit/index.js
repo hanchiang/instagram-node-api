d1a41d845c00f178be710b15ffcf3d8547877d70
Han Chiang <yaphc@hotmail.com>
2020-08-02 11:48:20 +0800

    Remove old queries test. Minor cleanup on tests

---
+++ tests/unit/index.js
@@ -0,0 +1,8 @@
+const { expect } = require('chai');
+
+const db = require('../../db');
+const { mockUser, mockUpdatedUser } = require('../data/user');
+
+// TODO:
+describe('Unit tests', () => {
+});
