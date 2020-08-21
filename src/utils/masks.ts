2a2aadc09b47af019368857973ca383063403d82
Han Chiang <yaphc@hotmail.com>
2020-08-21 17:16:43 +0800

    Add and adjust post masks

--- src/utils/masks.ts
+++ src/utils/masks.ts
@@ -1,16 +1,20 @@
 export const postMask = [
   'id',
-  'display_url',
+  'owner',
   'is_video',
+  'video_url',
+  'display_url',
+  'video_view_count',
+  'edge_media_to_comment',
   'edge_media_to_tagged_user',
   'edge_media_to_caption',
   'shortcode',
-  'edge_media_to_comment',
+  'comments_disabled',
   'taken_at_timestamp',
   'edge_media_to_sponsor_user',
   'edge_media_preview_like',
   'location',
-  'video_view_count',
+  'dimensions',
   'edge_sidecar_to_children',
 ];
 
